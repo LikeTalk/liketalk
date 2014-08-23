e00e0dca2b2b6c46f2c2d8c9befee0ae84afba38
Hans Jung <yhansjung@gmail.com>
2014-08-23 22:36:36 +0900

    모델 기획 및 가다 완성

--- apps/models.py
+++ apps/models.py
@@ -1,4 +1,25 @@
-"""
-models.py
+from apps import db
 
-"""
+class User(db.Model):
+    _id = db.Column(db.String(255), primary_key = True)
+    email = db.Column(db.String(255))
+    password = db.Column(db.String(255))
+    join_date = db.Column(db.DateTime(), default = db.func.now())
+
+
+class Match(db.Model):
+    match_id = db.Column(db.Integer, primary_key = True)
+
+
+class Candidate(db.Model):
+    Cand_id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(255))
+    school = db.Column(db.String(255))
+
+
+class Comment(db.Model):
+    Comment_id = db.Column(db.Integer, primary_key = True)
+    Comment_pswd = db.Column(db.String(255))
+    content = db.Column(db.Text())
+    date_created = db.Column(db.DateTime(), default=db.func.now())
+    likecount = db.Column(db.Integer, default = 0, nullable = False)
