a3b21fed8e022e652902899dabf940e739d77daf
LeeSeokmin <essemfly@gmail.com>
2014-08-23 19:25:34 +0900

    Reset All

--- apps/__init__.py
+++ apps/__init__.py
@@ -3,17 +3,9 @@ Initialize Flask app
 
 """
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_script import Manager
-from flask_migrate import Migrate, MigrateCommand
 
 
 app = Flask('apps')
 app.config.from_object('apps.settings.Production')
 
-db = SQLAlchemy(app)
-manager = Manager(app)
-migrate = Migrate(app,db)
-manager.add_command('db',MigrateCommand)
-
-import views, models
+import views
--- apps/views.py
+++ apps/views.py
@@ -2,17 +2,14 @@
 from flask import render_template, request
 from apps import app
 
-
 @app.route('/', methods=['GET'])
 def article_list():
-    return render_template("index.html")
+    return render_template("home.html")
 
 #
 # @error Handlers
 #
 # Handle 404 errors
-
-
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
