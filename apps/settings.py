4fd999717cbec5dc372f8ee21f03cda88302921e
LeeSeokmin <essemfly@gmail.com>
2014-08-25 02:47:24 +0900

    app.yaml

--- apps/settings.py
+++ apps/settings.py
@@ -8,13 +8,13 @@ Configuration for Flask app
 
 class Config(object):
     # Set secret key to use session
-    SECRET_KEY = "hanstobegreat"
+    SECRET_KEY = "policyinformer"
     debug = False
 
 
 class Production(Config):
     debug = True
     CSRF_ENABLED = False
-    ADMIN = "icu.20071102@gmail.com"
-    SQLALCHEMY_DATABASE_URI = 'mysql+gaerdbms:///liketalk?instance=hanstobegreat:myhans-data'
+    ADMIN = "essemfly@gmail.com"
+    SQLALCHEMY_DATABASE_URI = 'mysql+gaerdbms:///Liketalk?instance=policyinformer:essemfly0803'
     migration_directory = 'migrations'
